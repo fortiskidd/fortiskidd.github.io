b2c9b369ab91c7636ec68c835bfbfaa70f83c899
fortiskidd <chevaughn14@gmail.com>
2016-11-02 21:40:06 -0500

    Sliding puzzle

---
+++ project 2/fifteen.js
@@ -0,0 +1,51 @@
+window.onload=function(){
+	order();
+	var btn = document.createElement("BUTTON");        // Create a <button> element
+	var t = document.createTextNode("Finished");       // Create a text node
+	btn.appendChild(t);                                // Append the text to <button>
+	var butt = document.getElementById("controls").appendChild(btn);  
+	butt.onclick = win;
+	document.getElementById("shufflebutton").addEventListener('click', shuffle);
+
+};	
+
+function order(){
+	var puzzle=document.getElementById("puzzlearea");
+	var pieces=puzzle.children;
+	var xSpace = "300px";
+	var ySpace = "300px";
+	for(var j=0;j<pieces.length;j++){
+		pieces[j].className= "puzzlepiece";
+		pieces[j].style.left = (j%4*100)+'px';
+		pieces[j].style.top = (parseInt(j/4)*100) + 'px';
+		pieces[j].style.backgroundPosition= '-' + pieces[j].style.left + ' ' + '-' + pieces[j].style.top;
+		document.getElementById("puzzlearea").addEventListener("mouseover", function() { puzzle.classList.add("movablepiece")});
+		document.getElementById("puzzlearea").addEventListener("mouseout", function() { puzzle.classList.remove("movablepiece")});
+		//pieces[j].addEventListener('click',Move);	*/
+
+	}
+}
+
+function Move() {
+	
+}
+
+function shuffle() {
+	alert('Under Development');  /*
+	for (var i =pieces.length - 1; i > 0;i--) {
+		var j = Math.floor(Math.random() * (i + 1));
+		var temp = pieces[i];
+		pieces[i] = pieces[j];
+		pieces[j] = temp;
+	}
+	return pieces;*/
+}
+
+    function win()
+{
+	var body = document.getElementsByTagName('body');
+	body[0].style.backgroundImage = 'url(" C:/Users/chevaughn/Desktop/meh/winner.jpg")';
+	alert('You Are A Winner!!');
+}
+
+
